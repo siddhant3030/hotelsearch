fd00755a498e90038e7d7171218f31c4f16bfd63
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-24 05:18:06 +0530

    navbar added to every page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Home from "./pages/Home";
 import Error from "./pages/Error";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
-import {Route, Switch} from 'react-router-dom'
-
+import {Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return  <>
+  <Navbar />
   <Switch>
   <Route exact path="/" component={Home}/>
   <Route exact path="/rooms/" component={Rooms}/>
