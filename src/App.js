acd476d8c480f1deb2f57145e96d9b849454d73c
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-24 04:16:12 +0530

    imports of component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 // import './App.css';
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+
 
 function App() {
   return  <>Hello from app</>
