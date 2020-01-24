a23c4a7cc4567bd28a91847c8a3fd9cb736aa0f4
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-24 05:34:14 +0530

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
+    state={
+        isOpen: false
+    }
+
+    handleToggle = () => {
+        this.setState({isOpen:!this.state.isOpen})
+    }
     render() {
-        return (
-            <div>
-                
+        return <nav className="navbar">
+            <div className="nav-centre">
+                <div className="nav-header">
+                    <Link to="/">
+                        <img src={logo} alt="Beach Resort" />
+                        <button type="button" className="nav-btn">
+                            <FaAlignRight className="nav-icon"/> 
+                        </button>
+                    </Link>
+                </div>
             </div>
-        )
+        </nav>
     }
 }
