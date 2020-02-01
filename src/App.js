7fccac6fd1c3b3c3c0b71241a34a66c739e9d3be
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-02 00:58:43 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -8,15 +8,8 @@ import {Route, Switch} from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return  <>
-  <Navbar />
-  <Switch>
-  <Route exact path="/" component={Home}/>
-  <Route exact path="/rooms/" component={Rooms}/>
-  <Route exact path="/rooms/:slug" component={SingleRoom}/>
-  <Route component={Error} />
-  </Switch>
-  </>
+  return
+
 }
 
 export default App;
--- src/pages/Rooms.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Rooms() {
-    return (
-        <div>
-            Hello from Rooms
-        </div>
-    )
-}
