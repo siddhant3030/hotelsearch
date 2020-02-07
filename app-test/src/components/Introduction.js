8cc2b14d6b9cf58325841b96d78202d07c76d037
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-08 01:25:25 +0530

    handle state in class component

--- app-test/src/components/Introduction.js
+++ app-test/src/components/Introduction.js
@@ -12,11 +12,17 @@ export default class Introduction extends Component {
         this.handleInput = this.handleInput.bind(this);
     }
 
+    handleInput = (e) => {
+        const { value } = e.target;
+        console.log("value:", value )
+        this.setState({ userInput: value })
+     }
+
  render() {
      return (
         <div>
             <input value={this.state.userInput} onChange={this.handleInput}/>
-            <h1>{this.state.message}</h1>
+            <h1>{this.state.userInput}</h1>
         </div>
      );
  }
