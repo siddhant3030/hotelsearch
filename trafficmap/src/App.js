50951f664ebc99f05605149a605c84f83ec05305
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-08 04:21:34 +0530

    state

--- trafficmap/src/App.js
+++ trafficmap/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { 
   Collapse,
@@ -14,14 +14,25 @@ import {
   DropdownItem,
   NavbarText
 } from 'reactstrap';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props)
 
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  }
+}
 export default App;
