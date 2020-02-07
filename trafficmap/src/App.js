e905789375aa3266ac1d7cf6c8fdcb7e140da309
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-08 04:12:15 +0530

    installed reactstrap

--- trafficmap/src/App.js
+++ trafficmap/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { } from 'reactstrap';
 import './App.css';
 
 function App() {
@@ -8,16 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Traffic 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
