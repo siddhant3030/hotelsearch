a1b93ed450da8fafbba2e04115b8a8fca0c347e4
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-08 17:30:41 +0530

    refactoring

--- trafficmap/src/App.js
+++ trafficmap/src/App.js
@@ -42,28 +42,8 @@ class App extends Component {
         <Collapse isOpen={this.state.isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              1000 Traffic incident today
             </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
           </Nav>
           <NavbarText>Simple Text</NavbarText>
         </Collapse>
