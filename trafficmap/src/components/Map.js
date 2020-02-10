9305ffe8a8e810fabb6470dc4ed83ddcad509c7d
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-10 20:37:19 +0530

    added extra coordinate

--- trafficmap/src/components/Map.js
+++ trafficmap/src/components/Map.js
@@ -13,19 +13,32 @@ export default class Map extends Component {
                 zoom: 8,
                 latitude: 53.5444,
                 longitude: -113.4909
-            }
+            },
+            coords: [
+                { latitude: 51.5454, longitude: -113.3434 },
+                { latitude: 52.5454, longitude: -113.4947 },
+                { latitude: 53.5454, longitude: -113.3941 },
+                { latitude: 54.5454, longitude: -113.4994 },
+                { latitude: 55.5454, longitude: -113.3234 },
+                { latitude: 56.5454, longitude: -113.3455 },
+                { latitude: 57.5454, longitude: -113.3460 }
+            ]
         }
     }
 
     render() {
+        const { coords } = this.state;
         return (
            <MapGL
            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
            {...this.state.viewport}
            onViewportChange={viewport => this.setState({ viewport })}>
-                <Marker latitude={53.5225} longitude={-113.6242}>
+               { coords.map(coord => (
+                <Marker>
                     <Pin />
                 </Marker>
+               )) }
+              
            </MapGL>
         )
     }
